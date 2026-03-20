26724b810433e65e57a7c5b06b599a898e7fb5f4
MuriloX06 <muriloabreucastelobranco@gmail.com>
2026-03-20 17:25:45 -0300

    Atualização automática das configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,3 +9,27 @@ function fish_greeting
 end
 
 fish_add_path /home/murilo/.spicetify
+
+function sync_dots
+    # Vai para o QG dos dotfiles
+    cd ~/dotfiles
+    
+    # Prepara todos os arquivos modificados
+    git add .
+    
+    # Se você digitar uma mensagem, ele usa. Se não, usa uma padrão!
+    if test -z "$argv"
+        git commit -m "Atualização automática das configs"
+    else
+        git commit -m "$argv"
+    end
+    
+    # Envia pro GitHub
+    git push -u origin main
+    
+    # Volta para a pasta em que você estava antes de rodar o comando
+    cd -
+    
+    # Mostra a mensagem de vitória
+    echo "✅ Backup dos dotfiles enviado para o GitHub com sucesso!"
+end
